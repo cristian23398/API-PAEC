864b014223258bdacbc368d3de5140e3fc1f3576
oscar2454-del <oscar24540@cbtis75.edu.mx>
2026-04-21 17:38:15 -0600

    C4

--- Api1.py
+++ Api1.py
@@ -1,16 +1,11 @@
 from fastapi import FastAPI
-from db import collection
+from db import collection 
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"mensaje": "API conectada a MongoDB"}
-
 @app.get("/estudiantes")
-async def obtener_estudiantes():
+async def estudiantes():
     datos = []
-
     cursor = collection.find()
 
     async for doc in cursor:
