4bb7604ae250a9628982083390edc19314ae9337
oscar2454-del <oscar24540@cbtis75.edu.mx>
2026-04-21 19:20:05 -0600

    C12

--- Api1.py
+++ Api1.py
@@ -3,19 +3,18 @@ from db import collection
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"mensaje": "API conectada a MongoDB"}
-
 @app.get("/estudiantes")
 async def obtener_estudiantes():
-
-    datos = []
-
-    cursor = collection.find()
-
-    async for doc in cursor:
-        doc["_id"] = str(doc["_id"])
-        datos.append(doc)
-
-    return datos
+    try:
+        datos = []
+        # Agregamos un log para ver en Render si el cursor encuentra algo
+        cursor = collection.find({}) 
+        
+        async for doc in cursor:
+            doc["_id"] = str(doc["_id"])
+            datos.append(doc)
+            
+        print(f"Se encontraron {len(datos)} estudiantes") # Esto se verá en los logs de Render
+        return datos
+    except Exception as e:
+        return {"error": str(e)}
