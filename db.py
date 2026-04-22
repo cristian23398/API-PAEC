45dbc4f98b50131ba912ab43ea7d144f79d25d67
oscar2454-del <oscar24540@cbtis75.edu.mx>
2026-04-22 15:19:04 -0600

    C16

--- db.py
+++ db.py
@@ -1,12 +1,8 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-# Intenta obtener la variable de Render
+# Render leerá la variable MONGO_URL que configuramos en su panel
 MONGO_URL = os.getenv("MONGO_URL")
-
-# Si por alguna razón Render no la lee, pégala aquí directamente (solo como prueba)
-if not MONGO_URL:
-    MONGO_URL = "mongodb+srv://oscar24540_db_user:RipHawai@cluster0.ohaz2mw.mongodb.net/MOLO?retryWrites=true&w=majority"
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["MOLO"]
