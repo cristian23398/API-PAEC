b4e32032fcadce159f6b89bc3bbfd7338a8f7648
oscar2454-del <oscar24540@cbtis75.edu.mx>
2026-04-21 18:31:18 -0600

    C8

--- Api1.py
+++ Api1.py
@@ -1,23 +1,21 @@
 from fastapi import FastAPI
-from db import db
+from db import collection
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"Mensaje": "¡FastAPI está vivo y funcionando!"}
-
-@app.post("/usuarios")
-async def crear_usuario():
-    usuario = {"nombre": "Oscar", "edad": 20}
-    result = await db.usuarios.insert_one(usuario)
-    return {"id": str(result.inserted_id)}
+    return {"mensaje": "API conectada a MongoDB"}
 
 @app.get("/estudiantes")
-async def estudiantes():
+async def obtener_estudiantes():
+
     datos = []
-    cursor = db.usuarios.find() # Cambié 'collection' por 'db.usuarios' para que coincida
+
+    cursor = collection.find()
+
     async for doc in cursor:
         doc["_id"] = str(doc["_id"])
         datos.append(doc)
+
     return datos
--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
-
-load_dotenv("mongo.env") # Indicamos el nombre exacto de tu archivo
 
 MONGO_URL = os.getenv("MONGO_URL")
+
 client = AsyncIOMotorClient(MONGO_URL)
-db = client.MOLO # Usa el nombre de tu DB que pusiste en la captura
+
+db = client.MOLO
+collection = db["1"]
