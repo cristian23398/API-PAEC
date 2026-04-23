7eafb445bd7b109dcbb5f2bcdd121d44e4bc691e
oscar2454-del <oscar24540@cbtis75.edu.mx>
2026-04-23 15:05:17 -0600

    C17

--- Api1.py
+++ Api1.py
@@ -7,14 +7,14 @@ app = FastAPI()
 async def obtener_estudiantes():
     try:
         datos = []
-        # Agregamos un log para ver en Render si el cursor encuentra algo
+        
         cursor = collection.find({}) 
         
         async for doc in cursor:
             doc["_id"] = str(doc["_id"])
             datos.append(doc)
             
-        print(f"Se encontraron {len(datos)} estudiantes") # Esto se verá en los logs de Render
+        print(f"Se encontraron {len(datos)} estudiantes")
         return datos
     except Exception as e:
         return {"error": str(e)}
