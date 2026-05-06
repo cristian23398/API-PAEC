e0acfd7efee2391bc2e595d3cacc6bfa61064363
cristian23398 <cristian23398@cbtis75.edu.mx>
2026-05-06 15:37:35 -0600

    C1

--- db.py
+++ db.py
@@ -5,5 +5,5 @@ from motor.motor_asyncio import AsyncIOMotorClient
 MONGO_URL = os.getenv("MONGO_URL")
 
 client = AsyncIOMotorClient(MONGO_URL)
-db = client["MOLO"]
+db = client["PAEC"]
 collection = db["1"]
