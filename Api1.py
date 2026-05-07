fff788baf720accd2ccbb0a60c1d8fc026246cf2
cristian23398 <cristian23398@cbtis75.edu.mx>
2026-05-07 15:36:29 -0600

    C6

--- Api1.py
+++ Api1.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import HTMLResponse
-from db import collection
 from fastapi.staticfiles import StaticFiles
+from bson import ObjectId
+from db import collection, db
 import os
 
 app = FastAPI()
@@ -32,3 +33,23 @@ async def obtener_estudiantes():
 
     except Exception as e:
         return {"error": str(e)}
+    
+@app.post("/agregar")
+async def agregar(nombre: str = Form(...)):
+    nombre = nombre.strip()
+    if not nombre:
+        raise HTTPException(status_code=400, detail="Nombre vacío")
+    res = await collection.insert_one({"nombre": nombre})
+    return {"id": str(res.inserted_id), "nombre": nombre}
+
+@app.delete("/eliminar/{id}")
+async def eliminar(id: str):
+
+    try:
+        obj_id = ObjectId(id)
+    except:
+        raise HTTPException(status_code=400, detail="ID inválido")
+        
+    await collection.delete_one({"_id": obj_id})
+    await db.asistencias.delete_many({"estudiante_id": id})
+    return {"mensaje": "borrado"}
